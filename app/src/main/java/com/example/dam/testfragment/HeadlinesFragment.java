053f1ff83a73732567e55e966b188c0322195e68
dam <240242398@qq.com>
2016-03-08 11:52:19 +0800

    添加 Fragment 的生命週期順序

--- app/src/main/java/com/example/dam/testfragment/HeadlinesFragment.java
+++ app/src/main/java/com/example/dam/testfragment/HeadlinesFragment.java
@@ -1,6 +1,7 @@
 package com.example.dam.testfragment;
 
 
+import android.app.Fragment;
 import android.app.ListFragment;
 import android.content.Context;
 import android.os.Bundle;
@@ -26,42 +27,88 @@ public class HeadlinesFragment extends ListFragment {
         public void onArticleSelected(int position);
     }
 
+    /**
+     * It's not called because this method has been added in API 23.
+     * If you run your application on a device with API 23 (marshmallow)
+     * then onAttach(Context) will be called.
+     * On all previous Android Versions onAttach(Activity) will be called.
+     *
+     * @param context
+     */
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_headlines, container, false);
+    public void onAttach(Context context) {
+        super.onAttach(context);
     }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1,  Ipsum.Headlines));
-    }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-
-        if (getFragmentManager().findFragmentById(R.id.article_fragment) != null){
-            getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
-        }
-    }
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
 
         try {
             mCallBack = (onHeadlineSelectedListener) getActivity();
         } catch (ClassCastException e) {
             throw new ClassCastException(getActivity().toString() + " must implement OnHeadlineSelectedListener");
         }
+
+        setListAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, Ipsum.Headlines));
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_headlines, container, false);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        if (getFragmentManager().findFragmentById(R.id.article_fragment) != null) {
+            getListView().setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+        }
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+    }
+
+    /**
+     * 如果Fragment添加到了返回棧里，則不會調用onDestroy
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
     }
 
     @Override
     public void onListItemClick(ListView l, View v, int position, long id) {
-//        super.onListItemClick(l, v, position, id);
+
         mCallBack.onArticleSelected(position);
 
         getListView().setItemChecked(position, true);
